5782df214884c168610ccce04a8f3ff1f821f726
georgebergson <george_bergson@hotmail.com>
2022-10-01 09:34:28 -0400

    Menu Efeito Transição

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+ theme: {
+    extend: {
+      colors:{
+        'primary':'#0ae820',
+        'primary-dark':'#1f1f1f',
+        'fundo':'#F1F5F9',
+        'cinza-escuro': '#1f1f1f',
+        'verde': '#0ae820',
+        'verde-escuro': '#00bc0f'
+      }
+    },
+   
   },
   plugins: [],
 }
